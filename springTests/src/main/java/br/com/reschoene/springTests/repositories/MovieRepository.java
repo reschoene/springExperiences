0abd406ad2a4120d717533ba524944397f00c84c
reschoene <reschoene@gmail.com>
2020-12-27 17:50:19 -0300

    spring boot testing JPA ok

--- springTests/src/main/java/br/com/reschoene/springTests/repositories/MovieRepository.java
+++ springTests/src/main/java/br/com/reschoene/springTests/repositories/MovieRepository.java
@@ -4,6 +4,10 @@ import br.com.reschoene.springTests.entities.MovieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MovieRepository extends JpaRepository<MovieEntity, Long> {
+    List<MovieEntity> findByTitle(String name);
 }
--- springTests/src/test/java/br/com/reschoene/springTests/repositories/MovieRepositoryTest.java
+++ springTests/src/test/java/br/com/reschoene/springTests/repositories/MovieRepositoryTest.java
@@ -2,9 +2,12 @@ package br.com.reschoene.springTests.repositories;
 
 import br.com.reschoene.springTests.entities.MovieEntity;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Date;
 
 @DataJpaTest
 class MovieRepositoryTest {
@@ -12,18 +15,68 @@ class MovieRepositoryTest {
     private MovieRepository movieRepository;
 
     @Test
-    void save_PersistMovie_WhenSuccessful(){
+    @DisplayName("Save creates movie when successful")
+    void save_CreateMovie_WhenSuccessful(){
         var movie = createMovie();
-        var savedMovie = this.movieRepository.save(movie);
+        var createdMovie = this.movieRepository.save(movie);
 
-        Assertions.assertThat(savedMovie).isNotNull();
-        Assertions.assertThat(savedMovie.getId()).isNotNull();
-        Assertions.assertThat(savedMovie.getTitle()).isEqualTo(movie.getTitle());
+        Assertions.assertThat(createdMovie).isNotNull();
+        Assertions.assertThat(createdMovie.getId()).isNotNull();
+        Assertions.assertThat(createdMovie.getTitle()).isEqualTo(movie.getTitle());
+        Assertions.assertThat(createdMovie.getReleaseDate()).isEqualTo(movie.getReleaseDate());
+    }
+
+    @Test
+    @DisplayName("Save updates movie when successful")
+    void save_UpdatesMovie_WhenSuccessful(){
+        var movie = createMovie();
+        var createdMovie = this.movieRepository.save(movie);
+
+        Date dt = new Date();
+
+        createdMovie.setTitle("New Title");
+        createdMovie.setReleaseDate(dt);
+        var updatedMovie = this.movieRepository.save(createdMovie);
+
+        Assertions.assertThat(updatedMovie).isNotNull();
+        Assertions.assertThat(updatedMovie.getId()).isNotNull();
+        Assertions.assertThat(updatedMovie.getTitle()).isEqualTo(createdMovie.getTitle());
+        Assertions.assertThat(updatedMovie.getReleaseDate()).isEqualTo(dt);
+    }
+
+    @Test
+    @DisplayName("Delete removes movie when successful")
+    void delete_RemovesMovie_WhenSuccessful(){
+        var movie = createMovie();
+        var createdMovie = this.movieRepository.save(movie);
+
+        this.movieRepository.delete(createdMovie);
+
+        var movieOptional = this.movieRepository.findById(createdMovie.getId());
+        Assertions.assertThat(movieOptional).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Find by Title returns list of movies when successful")
+    void findByTitle_ReturnsListOfMovies_WhenSuccessful(){
+        var movie = createMovie();
+        var createdMovie = this.movieRepository.save(movie);
+
+        var movies = this.movieRepository.findByTitle(createdMovie.getTitle());
+        Assertions.assertThat(movies).isNotEmpty().contains(createdMovie);
+    }
+
+    @Test
+    @DisplayName("Find by Title returns empty list when no movie was found")
+    void findByTitle_ReturnsEmptyList_WhenNoMovieWasFound(){
+        var movies = this.movieRepository.findByTitle("Untitled");
+        Assertions.assertThat(movies).isEmpty();
     }
 
     private MovieEntity createMovie(){
         var movieEntity = new MovieEntity();
         movieEntity.setTitle("Inception");
+        movieEntity.setReleaseDate(new Date());
         return movieEntity;
     }
 }
