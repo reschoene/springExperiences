31007c04e1b89bc8c034fbc85631ffeb72175576
reschoene <reschoene@gmail.com>
2020-12-29 11:48:47 -0300

    enhanced documentation

--- springTests/src/test/java/br/com/reschoene/springTests/controllers/MovieControllerTest.java
+++ springTests/src/test/java/br/com/reschoene/springTests/controllers/MovieControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.ConstraintViolationException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -35,30 +34,30 @@ public class MovieControllerTest {
     void setUp(){
         var oneMovieList = List.of(MovieCreator.createMovieToBeSaved());
         var moviePage = new PageImpl<>(oneMovieList);
-        BDDMockito.when(movieService.findAll(ArgumentMatchers.any()))
-                .thenReturn(moviePage);
+        BDDMockito.given(movieService.findAll(ArgumentMatchers.any()))
+                .willReturn(moviePage);
 
-        BDDMockito.when(movieService.findAllNonPageable())
-                .thenReturn(oneMovieList);
+        BDDMockito.given(movieService.findAllNonPageable())
+                .willReturn(oneMovieList);
 
-        BDDMockito.when(movieService.findById(ArgumentMatchers.anyLong()))
-                .thenReturn(Optional.of(MovieCreator.createValidMovie()));
+        BDDMockito.given(movieService.findById(ArgumentMatchers.anyLong()))
+                .willReturn(Optional.of(MovieCreator.createValidMovie()));
 
-        BDDMockito.when(movieService.findByTitle(ArgumentMatchers.anyString()))
-                .thenReturn(oneMovieList);
+        BDDMockito.given(movieService.findByTitle(ArgumentMatchers.anyString()))
+                .willReturn(oneMovieList);
 
-        BDDMockito.when(movieService.create(ArgumentMatchers.any(MovieEntity.class)))
-                .thenReturn(MovieCreator.createMovieToBeSaved());
+        BDDMockito.given(movieService.create(ArgumentMatchers.any(MovieEntity.class)))
+                .willReturn(MovieCreator.createMovieToBeSaved());
 
-        BDDMockito.when(movieService.update(ArgumentMatchers.any(MovieEntity.class)))
-                .thenReturn(MovieCreator.createValidUpdatedMovie());
+        BDDMockito.given(movieService.update(ArgumentMatchers.any(MovieEntity.class)))
+                .willReturn(MovieCreator.createValidUpdatedMovie());
 
         BDDMockito.doNothing().when(movieService).delete(ArgumentMatchers.anyLong());
     }
 
     @Test
-    @DisplayName("findAll returns list of movies inside page object when successful")
-    void findAll_ReturnsListOfMoviesInsidePageObject_WhenSuccessful(){
+    @DisplayName("findAll returns list of movies inside page object given successful")
+    void findAll_ReturnsListOfMoviesInsidePageObject_givenSuccessful(){
         String expectedTitle = MovieCreator.createMovieToBeSaved().getTitle();
         var moviePage = movieController.findAll(null).getBody();
 
@@ -72,8 +71,8 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("findAllNonPageable returns list of all movies when successful")
-    void findAllNonPageable_ReturnsListOfMoviesInsidePageObject_WhenSuccessful(){
+    @DisplayName("findAllNonPageable returns list of all movies given successful")
+    void findAllNonPageable_ReturnsListOfMoviesInsidePageObject_givenSuccessful(){
         String expectedTitle = MovieCreator.createMovieToBeSaved().getTitle();
         var movieList = movieController.findAllNonPageable().getBody();
 
@@ -86,8 +85,8 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("findById returns one movie when successful")
-    void findById_ReturnsMovie_WhenSuccessful(){
+    @DisplayName("findById returns one movie given successful")
+    void findById_ReturnsMovie_givenSuccessful(){
         long expectedId = MovieCreator.createValidMovie().getId();
         var movie = movieController.findById(expectedId).getBody();
 
@@ -99,10 +98,10 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("findById Throws ResponseStatusException When Id Was Not Found")
-    void findById_ThrowsResponseStatusException_WhenIdNotFound(){
-        BDDMockito.when(movieService.findById(ArgumentMatchers.anyLong()))
-                .thenReturn(Optional.empty());
+    @DisplayName("findById Throws ResponseStatusException given Id Was Not Found")
+    void findById_ThrowsResponseStatusException_givenIdNotFound(){
+        BDDMockito.given(movieService.findById(ArgumentMatchers.anyLong()))
+                .willReturn(Optional.empty());
 
         Assertions.assertThatExceptionOfType(ResponseStatusException.class)
                 .isThrownBy(() -> this.movieController.findById(-1))
@@ -110,8 +109,8 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("findByTitle returns list of movies that matched a given title when successful")
-    void findByTitle_ReturnsListOfMoviesMatchedTitle_WhenSuccessful(){
+    @DisplayName("findByTitle returns list of movies that matched a given title given successful")
+    void findByTitle_ReturnsListOfMoviesMatchedTitle_givenSuccessful(){
         String expectedTitle = MovieCreator.createMovieToBeSaved().getTitle();
         var movieList = movieController.findByTitle(expectedTitle).getBody();
 
@@ -123,10 +122,10 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("findByTitle returns an empty list of movies when title was not found")
-    void findByTitle_ReturnsEmptyList_WhenMovieWasNotFound(){
-        BDDMockito.when(movieService.findByTitle(ArgumentMatchers.anyString()))
-                .thenReturn(Collections.emptyList());
+    @DisplayName("findByTitle returns an empty list of movies given title was not found")
+    void findByTitle_ReturnsEmptyList_givenMovieWasNotFound(){
+        BDDMockito.given(movieService.findByTitle(ArgumentMatchers.anyString()))
+                .willReturn(Collections.emptyList());
 
         var movieList = movieController.findByTitle("").getBody();
 
@@ -135,8 +134,8 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("create returns one movie when successful")
-    void create_ReturnsOptionalMovie_WhenSuccessful(){
+    @DisplayName("create returns one movie given successful")
+    void create_ReturnsOptionalMovie_givenSuccessful(){
         var movie = MovieCreator.createMovieToBeSaved();
         var savedMovie = movieController.create(movie).getBody();
 
@@ -147,8 +146,8 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("update updates movie when successful")
-    void update_UpdatesMovie_WhenSuccessful(){
+    @DisplayName("update updates movie given successful")
+    void update_UpdatesMovie_givenSuccessful(){
         var movie = MovieCreator.createValidUpdatedMovie();
         var updatedMovie = movieController.update(movie).getBody();
 
@@ -158,8 +157,8 @@ public class MovieControllerTest {
     }
 
     @Test
-    @DisplayName("delete removes movie when successful")
-    void delete_RemovesMovie_WhenSuccessful(){
+    @DisplayName("delete removes movie given successful")
+    void delete_RemovesMovie_givenSuccessful(){
         Assertions.assertThatCode(() -> movieController.delete(1)).doesNotThrowAnyException();
 
         ResponseEntity<Void> response = movieController.delete(1);
